f86852ab1ca06ae57144d6ff35ec217cd0886d5a
mb <edu.markb@gmail.com>
2015-10-29 07:01:51 -0400

    installed Mockito

---
+++ CrownAndAnchorGame/src/src/tests/PayoutTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class payoutTest
+{
+  @Test
+  public void payouttest()
+  {
+    fail("Not yet implemented");
+  }
+}
