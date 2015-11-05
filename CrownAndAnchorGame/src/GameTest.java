a28e92589fb62c877080930aa3ff14f51b83a508
mb <edu.markb@gmail.com>
2015-11-04 23:19:25 -0500

    Bug one reproduced

--- CrownAndAnchorGame/src/GameTest.java
+++ CrownAndAnchorGame/src/GameTest.java
@@ -8,13 +8,11 @@ import org.junit.Test;
 public class GameTest
 {
   private Player player;
-  private Dice die1, die2, die3;
   
   @Before
   public void setUp() throws Exception
   {
     player = new Player("Fred", 100);
-    int bet = 5;
   }
 
 
@@ -36,15 +34,91 @@ public class GameTest
     Dice die3 = mock (Dice.class);
     
     // define return value
-    when (die1.roll()).thenReturn(DiceValue.CLUB);
-    when (die2.roll()).thenReturn(DiceValue.CLUB);
-    when (die3.roll()).thenReturn(DiceValue.ANCHOR);
     when (die1.getValue()).thenReturn(DiceValue.CLUB);
     when (die2.getValue()).thenReturn(DiceValue.CLUB);
     when (die3.getValue()).thenReturn(DiceValue.ANCHOR);
     
+    // play round
+    Game game = new Game (die1, die2, die3);
+    int bet = 5;
+    int balance = 100;
+    DiceValue pick = DiceValue.ANCHOR;
+    
+    int result = balance + game.playRound (player, pick, bet);
+    
     // test
+    assertEquals(result, player.getBalance());
+  }
+  @Test
+  public void twoMatch()
+  {
+    // create mock
+    Dice die1 = mock (Dice.class);
+    Dice die2 = mock (Dice.class);
+    Dice die3 = mock (Dice.class);
+    
+    // define return value
+    when (die1.getValue()).thenReturn(DiceValue.CLUB);
+    when (die2.getValue()).thenReturn(DiceValue.CLUB);
+    when (die3.getValue()).thenReturn(DiceValue.ANCHOR);
+    
+    // play round
+    Game game = new Game (die1, die2, die3);
+    int bet = 5;
+    int balance = 100;
     DiceValue pick = DiceValue.CLUB;
-    assertEquals(die1.getValue(), pick);
+    
+    int result = balance + game.playRound (player, pick, bet);
+    
+    // test
+    assertEquals(result, player.getBalance());
+  }
+  @Test
+  public void threeMatch()
+  {
+    // create mock
+    Dice die1 = mock (Dice.class);
+    Dice die2 = mock (Dice.class);
+    Dice die3 = mock (Dice.class);
+    
+    // define return value
+    when (die1.getValue()).thenReturn(DiceValue.CLUB);
+    when (die2.getValue()).thenReturn(DiceValue.CLUB);
+    when (die3.getValue()).thenReturn(DiceValue.CLUB);
+    
+    // play round
+    Game game = new Game (die1, die2, die3);
+    int bet = 5;
+    int balance = 100;
+    DiceValue pick = DiceValue.CLUB;
+    
+    int result = balance + game.playRound (player, pick, bet);
+    
+    // test
+    assertEquals(result, player.getBalance());
+  }
+  @Test
+  public void noMatch()
+  {
+    // create mock
+    Dice die1 = mock (Dice.class);
+    Dice die2 = mock (Dice.class);
+    Dice die3 = mock (Dice.class);
+    
+    // define return value
+    when (die1.getValue()).thenReturn(DiceValue.CLUB);
+    when (die2.getValue()).thenReturn(DiceValue.CLUB);
+    when (die3.getValue()).thenReturn(DiceValue.CLUB);
+    
+    // play round
+    Game game = new Game (die1, die2, die3);
+    int bet = 5;
+    int balance = 100;
+    DiceValue pick = DiceValue.HEART;
+    
+    int result = balance - bet + game.playRound (player, pick, bet);
+    
+    // test
+    assertEquals(result, player.getBalance());
   }
 }
